0605f1596f204cfbcef0fdab662043074da6afcf
hasanhincal <hasanfatime14@gmail.com>
2024-08-13 12:15:43 +0300

    edit

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,8 @@
-import { fuchsia } from "tailwindcss/colors";
-import NextProgress from "nextjs-progressbar";
 import "@/app/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <NextProgress color={fuchsia[500]} height={4} />
       <Component {...pageProps} />
     </>
   );
